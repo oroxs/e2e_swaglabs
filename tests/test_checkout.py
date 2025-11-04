a61bdfef7106347a2d4d1f924d1fcabf9e7f4117
Vedy Aditya Nugraha <Vedy_A103@mitrais.com>
2025-11-04 09:44:21 +0700

    update test checkout

---
+++ tests/test_checkout.py
@@ -0,0 +1,42 @@
+import allure
+from pages.login import LoginPage
+from pages.product import ProductPage
+from pages.cart import CartPage
+from pages.checkout_info import CheckoutInfoPage
+from pages.checkout_overview import CheckoutOverviewPage
+from pages.checkout_complete import CheckoutCompletPage
+
+@allure.title("Checkout Test")
+@allure.description("Test Untuk Melakukan Checkout") 
+@allure.severity(allure.severity_level.CRITICAL) 
+
+def test_login(setup):    
+   login = LoginPage(setup)
+   product = ProductPage(setup)
+   cart = CartPage(setup)
+   info = CheckoutInfoPage(setup)
+   overview = CheckoutOverviewPage(setup)
+   complete = CheckoutCompletPage (setup)
+
+   login.inputUsername('standard_user')
+   login.inputPassword('secret_sauce')
+   login.clickButtonLogin()
+   product.check_product_page()
+   product.select_product()
+   product.click_icon_cart()
+   cart.check_title()
+   cart.click_checkout()
+   info.check_title()
+   info.inputFirstName('Vedy')
+   info.inputLastName('Aditya')
+   info.inputPostalCode('123123')
+   info.clickContinue()
+   overview.check_title()
+   overview.scroll_down()
+   overview.check_total_price()
+   overview.check_total_tax()
+   overview.check_grand_total()
+   overview.click_finish_button()
+   complete.check_title()
+
+
